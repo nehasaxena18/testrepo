cfa2b94d11bd46f73fcaca462f14ae8048970e2a
Gaikwad, Prashant <prashant.gaikwad@accenture.com>
2015-08-30 17:41:37 -0500

    Added Client code to fetch the code

--- SelfHost/Program.cs
+++ SelfHost/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.SelfHost;
 using System.Configuration;
+using System.Net.Http;
 
 namespace SelfHost
 {
@@ -25,11 +26,30 @@ namespace SelfHost
             using (HttpSelfHostServer server = new HttpSelfHostServer(config))
             {
                 server.OpenAsync().Wait();
-
+                var pathroot = server.Configuration.VirtualPathRoot;
+                
                 Console.WriteLine("Listening for HTTP requests.");
+                Console.WriteLine(pathroot.ToString());
                 Console.WriteLine("(Run the ClientApp project to send requests).");
                 Console.WriteLine();
+
+             
+
                 Console.WriteLine("Press Enter to quit.");
+
+
+                Console.WriteLine("(Runinng the ClientApp,Getting all products).");
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["applicationuri"]);
+
+                HttpResponseMessage resp = client.GetAsync("api/products").Result;
+                resp.EnsureSuccessStatusCode();
+
+                var products = resp.Content.ReadAsAsync<IEnumerable<SelfHost.Product>>().Result;
+                foreach (var p in products)
+                {
+                    Console.WriteLine("{0} {1} {2} ({3})", p.Id, p.Name, p.Price, p.Category);
+                }
                 Console.ReadLine();
             }
         }
